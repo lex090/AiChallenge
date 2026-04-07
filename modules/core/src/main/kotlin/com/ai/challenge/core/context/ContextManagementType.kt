9c8183f4e617ca8799e3c6bdc69a9908c9ae9f7a
Ilya Lisitsa <lex7555@yandex.ru>
2026-04-08 00:55:12 +0300

    feat(core): add SlidingWindow variant to ContextManagementType

--- modules/core/src/main/kotlin/com/ai/challenge/core/context/ContextManagementType.kt
+++ modules/core/src/main/kotlin/com/ai/challenge/core/context/ContextManagementType.kt
@@ -3,4 +3,5 @@ package com.ai.challenge.core.context
 sealed interface ContextManagementType {
     data object None : ContextManagementType
     data object SummarizeOnThreshold : ContextManagementType
+    data object SlidingWindow : ContextManagementType
 }
